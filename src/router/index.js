5839ebebe92cd27436c075a02bfe57d240a0ff48
alextbz <alextimurboyz@gmail.com>
2022-12-04 00:30:23 +0700

    uploadVue
    upload file vue

---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/product',
+    name: 'product',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/ProductView.vue')
+  },
+  {
+    path: '/shoppingcart',
+    name: 'shoppingcart',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/ShpngcartView.vue')
+  },
+  {
+    path: '/success',
+    name: 'success',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/SuccessView.vue')
+  },
+  
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <headerKu />
+    <HeroView />
+    <WomenView />
+    <InstaView />
+    <XpatnerView />
+    <FooterView/>
+
+   
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import headerKu from "@/components/HeaderView.vue";
+import HeroView from "@/components/HeroView.vue";
+import WomenView from "@/components/WomenView.vue";
+import InstaView from "@/components/InstagramView.vue";
+import XpatnerView from "@/components/XpatnerView.vue";
+import FooterView from "@/components/FooterView.vue";
+
+
+
+
+
+export default {
+  name: "HomeView",
+  components: {
+    headerKu,
+    HeroView,
+    WomenView,
+    InstaView,
+    XpatnerView,
+    FooterView,
+
+    
+   
+  },
+};
+</script>
